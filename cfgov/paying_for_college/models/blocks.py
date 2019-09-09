310a56a75fd81192c6e3aa9b8c87cbceba90ac7d
higs4281 <higs4281@gmail.com>
2019-09-09 12:45:31 -0400

    Add quiz fields and rebase

--- cfgov/paying_for_college/models/blocks.py
+++ cfgov/paying_for_college/models/blocks.py
@@ -4,7 +4,8 @@ from wagtail.wagtailcore import blocks
 class QuizAnswers(blocks.StructBlock):
     answers = blocks.ListBlock(
         blocks.StructBlock([
-            ('choice_letter', blocks.CharBlock(max_length=20)),
+            ('choice_letter', blocks.CharBlock(
+                max_length=20, required=False)),
             ('answer_choice', blocks.CharBlock(max_length=500)),
             ('answer_response', blocks.RichTextBlock(
                 features=[
@@ -30,6 +31,8 @@ class GuidedQuiz(blocks.StructBlock):
     )
     questions = blocks.ListBlock(
         blocks.StructBlock([
+            ('subtitle', blocks.CharBlock(
+                max_length=500, required=False)),
             ('question', blocks.RichTextBlock(
                 features=[
                     'ol', 'ul', 'bold', 'italic',
@@ -40,6 +43,13 @@ class GuidedQuiz(blocks.StructBlock):
             )),
             ('answers', QuizAnswers()),
         ]))
+    summary = blocks.RichTextBlock(
+        features=[
+            'ol', 'ul', 'bold', 'italic', 'link', 'image', 'document-link',
+        ],
+        blank=True,
+        required=False,
+    )
 
     class Meta:
         icon = 'grip'
