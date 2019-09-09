cbaa1dfbbaaea42034514754a9748d2afbd745e8
Chuck Werner <mistergone@gmail.com>
2019-09-09 16:40:31 -0400

    First pass at P4C quiz styles
    - An initial pass at the P4C quiz styles and interactions

--- gulp/config.js
+++ gulp/config.js
@@ -67,6 +67,7 @@ module.exports = {
     },
     otherBuildTriggerFiles: [
       paths.unprocessed + '/css/**/*.less',
+      paths.unprocessed + '/apps/**/css/**/*.less',
       paths.modules,
       './config/**/*.js',
       './gulp/**/*.js'
