70e5af2e04e96877aae01bab470191bf1254578f
Chuck Werner <mistergone@gmail.com>
2019-09-13 13:51:46 -0400

    New LESS and JS for P4C
    - New Paying for College LESS and JS for the quizes and the college costs tool

--- cfgov/unprocessed/apps/paying-for-college/js/quiz.js
+++ cfgov/unprocessed/apps/paying-for-college/js/quiz.js
@@ -10,38 +10,60 @@ const quizHandler = function( container ) {
 
 	const handleButtons = () => {
 		handler.questions.forEach( ( question ) => {
-			const buttons = question.querySelectorAll( '.quiz__answer-choice' );
-			buttons.forEach( ( buttonArea ) => {
-				const input = buttonArea.querySelector( '.a-label' );
-				input.addEventListener( 'click', ( event ) => {
-					// TODO - Refactor this once we have IDs working
-					const answer = closest( event.target, '.quiz__answer' );
+			const buttons = question.querySelectorAll( '.quiz__response-button' );
+			buttons.forEach( ( button ) => {
+				button.addEventListener( 'click', ( event ) => {
+					event.preventDefault();
 					const answerSection = closest( event.target, '.quiz__answers' );
-					const radioButtons = answerSection.querySelectorAll( 'input[type="radio"]' );
-					radioButtons.forEach( ( button ) => {
-						button.checked = false;
-					} );
+					const responseTarget = answerSection.querySelector( '.quiz__response-target' );
+					const checked = answerSection.querySelector( 'input:checked' );
+					const checkedAnswer = closest( checked, '.quiz__answer' );
+					const checkedResponse = checkedAnswer.querySelector( '.quiz__answer-response' );
+					const clone = checkedResponse.cloneNode( true );
 
-					const radioResponses = answerSection.querySelectorAll( '.quiz__answer-response' );
-					radioResponses.forEach( ( elem ) => {
-						
-						if ( !elemContainsElem( answer, elem ) ) {
-							hide( elem );
-							// slide( 'up', elem );
-						}
-					} );
-
-					const clickedButton = closest( event.target, '.quiz__answer')
-						.querySelector( 'input[type="radio"]' );
-					clickedButton.checked = true;
-
-					const response = answer.querySelector( '.quiz__answer-response' );
-					if ( response !== null ) {
-						// show( response );
-						slide( 'down', response );
+					// clear target area & append clone
+					let first = responseTarget.firstElementChild;
+					while ( first ) {
+						responseTarget.removeChild( first );
+						first = responseTarget.firstElementChild;
 					}
+					responseTarget.appendChild( clone );
+					
+					slide( 'down', clone );
 
 				} );
+
+
+				// const input = buttonArea.querySelector( '.a-label' );
+				// input.addEventListener( 'click', ( event ) => {
+				// 	// TODO - Refactor this once we have IDs working
+				// 	const answer = closest( event.target, '.quiz__answer' );
+				// 	const answerSection = closest( event.target, '.quiz__answers' );
+				// 	const radioButtons = answerSection.querySelectorAll( 'input[type="radio"]' );
+				// 	radioButtons.forEach( ( button ) => {
+				// 		button.checked = false;
+				// 	} );
+
+				// 	const radioResponses = answerSection.querySelectorAll( '.quiz__answer-response' );
+				// 	radioResponses.forEach( ( elem ) => {
+						
+				// 		if ( !elemContainsElem( answer, elem ) ) {
+				// 			hide( elem );
+				// 			// slide( 'up', elem );
+				// 		}
+				// 	} );
+
+				// 	const clickedButton = closest( event.target, '.quiz__answer')
+				// 		.querySelector( 'input[type="radio"]' );
+				// 	clickedButton.checked = true;
+
+				// 	const response = answer.querySelector( '.quiz__answer-response' );
+				// 	if ( response !== null ) {
+				// 		// show( response );
+				// 		slide( 'down', response );
+				// 	}
+
+				// } );
 			} );
 		} );
 	}
@@ -50,6 +72,13 @@ const quizHandler = function( container ) {
 		// hide responses
 		handler.responses.forEach( ( response ) => {
 			hide( response );
+		} );
+
+		// TODO - Remove this when we get IDs working
+		const qsecs = document.querySelectorAll( '.quiz__answer-choice' );
+		qsecs.forEach( ( div, i ) => {
+			div.querySelector( 'input' ).setAttribute( 'id', 'answerChoice' + i );
+			div.querySelector( 'label' ).setAttribute( 'for', 'answerChoice' + i );
 		} );
 
 		handleButtons();
