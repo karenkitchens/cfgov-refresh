35dedfbf4c1d366fadfdffd4c3affdb9d540c9cc
el-mapache <adam.biagianti@gmail.com>
2019-10-31 13:45:15 -0700

    Splits out print table into view

--- cfgov/unprocessed/js/routes/practitioner-resources/resources-youth-employment-programs/buying-a-car/index.js
+++ cfgov/unprocessed/js/routes/practitioner-resources/resources-youth-employment-programs/buying-a-car/index.js
@@ -2,22 +2,30 @@ import checkbox from './templates/checkbox';
 import checklistGroupView from './checklist-group';
 import checklistMap from './models/checklist-map';
 import printButtonView from '../../../../../apps/youth-employment-success/js/views/print-button'
+import printTableView from './print-table';
 import selectedItems from './models/selected-items';
-import tableRow from './templates/table-row';
 import updateExpandableButtonText from './expandables';
 
 
 const CHECKLIST_GROUP_SELECTOR = 'm-checklist-group';
 const PRINT_BUTTON_SELECTOR = 'js-cbg-print';
 
-const GOALS_TABLE_BODY_SELECTOR = 'cbg-print-goals-body';
-const NEXT_STEPS_TABLE_BODY_SELECTOR = 'cbg-next-steps-body';
+const GOALS_TABLE_SELECTOR = 'cbg-print-goals';
+const NEXT_STEPS_TABLE_SELECTOR = 'cbg-print-next-steps';
 
 const cbgJSONEl = document.getElementById( 'car-buying-guide-json' );
 const cbgChecklistData = JSON.parse( cbgJSONEl.innerHTML );
 
 const items = selectedItems();
 const checklistLookup = checklistMap(cbgChecklistData);
+
+updateExpandableButtonText();
+
+checklistGroupView(
+  document.querySelector( `.${ CHECKLIST_GROUP_SELECTOR }` ), {
+    selectedItems: items
+  }
+).init();
 
 printButtonView(
   document.querySelector( `.${ PRINT_BUTTON_SELECTOR }` ), {
@@ -26,46 +34,16 @@ printButtonView(
   }
 ).init();
 
-checklistGroupView(
-  document.querySelector( `.${ CHECKLIST_GROUP_SELECTOR }` ), {
-    selectedItems: items
-  }
-).init();
-
-updateExpandableButtonText();
-
-const TableBuilder = {
-  buildBody(rows) {
-
-  },
-  buildRow(contents) {
-    return tableRow(content);
-  }
-}
-
-function buildTableBody( contents ) {
-  const fragment = document.createDocumentFragment();
-  const temp = document.createElement( 'table' );
-  const tempBody = document.createElement( 'tbody' );
-
-  temp.appendChild( tempBody );
-
-  contents.reduce((rows, rowContent) => {
-    return `${rows}${tableRow(rowContent)}`;
-  }, '')
-
-  temp.innerHTML = contents;
-
-  const rows = Array.prototype.slice.call( temp.querySelectorAll( 'tr' ) );
-
-  rows.forEach( row => fragment.appendChild( row ) );
-
-  return fragment;
-}
+const goalsTableView = printTableView(
+  document.querySelector(`.${GOALS_TABLE_SELECTOR}`)
+);
+const nextStepsTableView = printTableView(
+  document.querySelector(`.${NEXT_STEPS_TABLE_SELECTOR}`)
+);
 
 function handlePrintChecklist() {
-  const checklistDataElements = items.elements();
-  const goalsTableContent = checklistDataElements.reduce( ( memo, item ) => {
+  const selectedChecklistItems = items.elements();
+  const goalsTableContent = selectedChecklistItems.reduce( ( memo, item ) => {
     const checklistItem = checkbox( item );
     const checklistItemDetail = checklistLookup.get(item);
 
@@ -74,24 +52,14 @@ function handlePrintChecklist() {
     return memo;
   }, [] );
 
-  const goalsTableFragment = buildTableBodyRows( goalsTableContent );
-
-  const goalsTable = document.querySelector( `.${ GOALS_TABLE_BODY_SELECTOR }` );
-  goalTables.innerHTML = '';
-  goalsTable.appendChild( goalsTableFragment );
-
+  goalsTableView.render(goalsTableContent);
+  
   const remainingItems = checklistLookup.filterKeysBy((key) => {
-    return checklistDataElements.indexOf( key ) === -1 ;
+    return selectedChecklistItems.indexOf( key ) === -1 ;
   });
-  const nextStepsTableContent = remainingItems.reduce( ( memo, item ) => {
-    const checkListItem = checkbox( item );
-    const row = tableRow(checkListItem);
+  const nextStepsTableContent = remainingItems.map( ( item ) => {
+    return checkbox(item);
+  });
 
-    return `${ memo }${ row }`;
-  }, '' );
-  const nextStepsTableFragment = buildTableBodyRows( nextStepsTableContent );
-
-  const nextStepsTable = document.querySelector( `.${ NEXT_STEPS_TABLE_BODY_SELECTOR }` );
-  nextStepsTable.innerHTML = '';
-  nextStepsTable.appendChild( nextStepsTableFragment );
+  nextStepsTableView.render(nextStepsTableContent);
 }
